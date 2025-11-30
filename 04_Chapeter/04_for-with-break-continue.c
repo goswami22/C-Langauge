cef8d77f9648cf9053631886683d0f751d7576c8
Bhavesh <goswamibhavesh22@gmail.com>
2025-11-30 22:48:14 +0530

    new file added

---
+++ 04_Chapeter/04_for-with-break-continue.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+int main() {
+    // int count = 10;
+    for ( int i = 0; i < 10; i++)           // Loop variable initialization, Loop continuation      condition, Loop variable update  
+    {
+        // if(i == 5) {
+        //     break;                          // Exit the loop when i equals 5 (Exit the loop now)
+        // }
+
+        if(i == 5) {
+            continue;                       // Skip the current iteration when i equals 5 (Skip this iteration)
+        }
+        printf("%d\n", i);
+    }
+    
+    return 0;
+}
