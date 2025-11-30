48156ebb0dd3cea7bc3793f46854939529a29fcc
Bhavesh <goswamibhavesh22@gmail.com>
2025-11-30 21:14:51 +0530

    new file added

--- 04_Chapeter/06_natural-num.c
+++ 04_Chapeter/06_natural-num.c
@@ -3,12 +3,13 @@
 int main() {
     int i  = 1; // Initialize i to 1 starting from the first natural number
     int n;      // Variable to hold the upper limit input by the user
+    printf("Enter number\n");
     scanf("%d", &n);    
     do
     {
         printf("The value of i is: %d\n", i);
         i++;
-    } while (i <= nc);        
+    } while (i <= n);        
     
     return 0;
 }
