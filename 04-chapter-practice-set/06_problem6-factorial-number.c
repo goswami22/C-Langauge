f346be277d17ffbe60b5ea40b608fc45e5a396ed
Bhavesh <goswamibhavesh22@gmail.com>
2025-12-02 16:24:06 +0530

    new file added

--- 04-chapter-practice-set/06_problem6-factorial-number.c
+++ 04-chapter-practice-set/06_problem6-factorial-number.c
@@ -3,7 +3,10 @@
 #include <stdio.h>
 
 int main() {
+    //10! = 1 x 2 x 3 x 4 x 5 x 6 x 7 x 8 x 9 x 10 = 3628800;
+    // 9! = 1 x 2 x 3 x 4 x 5 x 6 x 7 x 8 x 9 = 362880;
     // 8! = 1 x 2 x 3 x 4 x 5 x 6 x 7 x 8 = 40320;
+    /// 7! = 1 x 2 x 3 x 4 x 5 x 6 x 7 = 5040;
 
     int product = 1;        // to store the factorial result        
     int n = 8;              // number to calculate factorial of 
@@ -16,14 +19,27 @@ int main() {
 
     // using while loop
     int factorial_num = 1;  // to store the factorial result      
-    int n1 = 10;        // number to calculate factorial of
+    int n1 = 8;        // number to calculate factorial of
 
     int i = 1;        // loop variable initialization   starting from 1
-    while (i <= n)    // Loop continuation condition
+    while (i <= n1)    // Loop continuation condition
     {
-        factorial_num *= n1;        // factorial_num = factorial_num * n1;      
+        factorial_num *= i;        // factorial_num = factorial_num * n1;      
         i++;                // Loop variable update 
     }
-    printf("The factorial of %d is %d\n", n , factorial_num);
+    printf("The factorial of %d is %d\n", n1 , factorial_num);
+
+    // using do while loop
+    int fact_num1 = 1;  // to store the factorial result = 
+    int num2 = 12;
+    int j = 1;
+    do {
+        fact_num1 *= j;
+        j++;
+    } while (j <= num2);
+
+    printf("The factrial number value of %d is %d\n", num2, fact_num1);
+
     return 0;
 }
+
